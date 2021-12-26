7bb4a75b28baa8f4abe21fe9d41aac9c4bcb1a08
Avdhoot <avdhootu.27@gmail.com>
2021-12-26 15:21:07 +0530

    Hand landmark detection

--- HandTracking.py
+++ HandTracking.py
@@ -4,8 +4,22 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+# formality to use mediapipe
+mpHands = mp.solutions.hands
+# following class needs image only in RGB format
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
+    RGBimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(RGBimg)
+    #print(results.multi_hand_landmarks)     # to print x, y, z coordinates of detected hand
+
+    # if hand detected
+    if results.multi_hand_landmarks:
+        for handLandMark in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLandMark, mpHands.HAND_CONNECTIONS)      # to show dots & lines on hand
 
     cv2.imshow("Image", img)
 
