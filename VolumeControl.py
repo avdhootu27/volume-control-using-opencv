a3337b1f6f5609fa8b475ef870f70164c23dcf30
Avdhoot <avdhootu.27@gmail.com>
2021-12-28 20:50:11 +0530

    Hand detection and line drawing between tip of thumb & index finger

--- VolumeControl.py
+++ VolumeControl.py
@@ -11,9 +11,21 @@ cap = cv2.VideoCapture(0)
 cap.set(3, camWidth)
 cap.set(4, camHeight)
 
+# creating objects of hand detector class
+detector = hdm.handDetector()
+
 while True:
     success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPositions(img, draw=False)
 
+    if len(lmList) != 0:        # draw circles
+        print(lmList[4], lmList[8])
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cv2.circle(img, (x1, y1), 8, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 8, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1,y1), (x2,y2), (0, 0, 255), 2)
 
     # calculating fps
     cTime = time.time()
